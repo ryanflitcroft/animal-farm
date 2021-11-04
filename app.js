ac7463d2e7234453c3458f42c2c4b23c2df39e66
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-11-04 14:35:14 -0700

    img audio files added in html, basic adjustments css, variables assigned to img audio elements in app.js

--- app.js
+++ app.js
@@ -6,3 +6,29 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  // A PLAN:
+// - 3 image elements, which can be accessed in DOM (getElementById);
+// - 3 audio elements, which can be accessed in DOM (getElementById);
+// - 1 element to indicate the image/audio event that has been accessed (span);
+//   -Images will have .eventListener('click', );
+//   -Audio will have .play();
+//   -Span.textContent will change based on which img/audio element was clicked;
+
+// ACCESS IMAGE ELEMENTS IN DOM, assign to const
+const catImage = document.getElementById('cat-image');
+const horseImage = document.getElementById('horse-image');
+const dogImage = document.getElementById('dog-image');
+
+// VALIDATE that const assignments exist
+console.log(catImage, horseImage, dogImage);
+// --TRUE
+
+// ACCESS AUDIO ELEMENTS IN DOM, assign to const
+const catAudio = document.getElementById('cat-audio');
+const horseAudio = document.getElementById('horse-audio');
+const dogAudio = document.getElementById('dog-audio');
+
+// VALIDATE that const assignments exist
+console.log(catAudio, horseAudio, dogAudio);
+// --TRUE
